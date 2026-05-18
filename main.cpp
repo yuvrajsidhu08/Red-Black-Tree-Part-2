972492237d03d510b604c449097911fe9a9eab2a
yuvrajsidhu08 <454009@bsd48.org>
2026-05-17 22:25:41 -0700

    commit

--- main.cpp
+++ main.cpp
@@ -25,22 +25,27 @@ private:
         return n;
     }
 
-    Node* insertBST(Node* r, Node* n) {
+    Node* insert(Node* r, Node* n) {
         if (!r) return n;
+
         if (n->data < r->data) {
-            r->left = insertBST(r->left, n);
+            r->left = insert(r->left, n);
             r->left->parent = r;
-        } else if (n->data > r->data) {
-            r->right = insertBST(r->right, n);
+        }
+        else if (n->data > r->data) {
+            r->right = insert(r->right, n);
             r->right->parent = r;
         }
+
         return r;
     }
 
     void leftRotate(Node*& r, Node*& x) {
         Node* y = x->right;
+
         x->right = y->left;
         if (y->left) y->left->parent = x;
+
         y->parent = x->parent;
 
         if (!x->parent) r = y;
@@ -53,8 +58,10 @@ private:
 
     void rightRotate(Node*& r, Node*& x) {
         Node* y = x->left;
+
         x->left = y->right;
         if (y->right) y->right->parent = x;
+
         y->parent = x->parent;
 
         if (!x->parent) r = y;
@@ -72,20 +79,32 @@ private:
 
             if (p == g->left) {
                 Node* u = g->right;
+
                 if (u && u->color == RED) {
-                    g->color = RED;
                     p->color = BLACK;
                     u->color = BLACK;
+                    g->color = RED;
                     x = g;
-                } else {
+                }
+                else {
                     if (x == p->right) {
                         leftRotate(r, p);
                         x = p;
                         p = x->parent;
                     }
+
                     rightRotate(r, g);
-                    Color t = p->color;
-                    p->color = g->color;
-                    g->color = t;
-                    x = p;
+
+                    p->color = BLACK;
+                    g->color = RED;
+                }
+            }
+            else {
+                Node* u = g->left;
+
+                if (u && u->color == RED) {
+                    p->color = BLACK;
+                    u->color = BLACK;
+                    g->color = RED;
+                    x = g;
                 }
