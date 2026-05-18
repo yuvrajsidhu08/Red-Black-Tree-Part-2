c381d42eca64bd9d63d627df7a4dcf6ff65395e5
yuvrajsidhu08 <454009@bsd48.org>
2026-05-17 21:13:44 -0700

    commit

--- main.cpp
+++ main.cpp
@@ -24,3 +24,29 @@ private:
         n->left = n->right = n->parent = NULL;
         return n;
     }
+
+    Node* insertBST(Node* r, Node* n) {
+        if (!r) return n;
+        if (n->data < r->data) {
+            r->left = insertBST(r->left, n);
+            r->left->parent = r;
+        } else if (n->data > r->data) {
+            r->right = insertBST(r->right, n);
+            r->right->parent = r;
+        }
+        return r;
+    }
+
+    void leftRotate(Node*& r, Node*& x) {
+        Node* y = x->right;
+        x->right = y->left;
+        if (y->left) y->left->parent = x;
+        y->parent = x->parent;
+
+        if (!x->parent) r = y;
+        else if (x == x->parent->left) x->parent->left = y;
+        else x->parent->right = y;
+
+        y->left = x;
+        x->parent = y;
+    }
