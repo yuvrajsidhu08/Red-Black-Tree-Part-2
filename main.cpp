8615272c5ca3b13911598d70194abc4a534b63d4
yuvrajsidhu08 <454009@bsd48.org>
2026-05-17 22:22:13 -0700

    commit

--- main.cpp
+++ main.cpp
@@ -50,3 +50,42 @@ private:
         y->left = x;
         x->parent = y;
     }
+
+    void rightRotate(Node*& r, Node*& x) {
+        Node* y = x->left;
+        x->left = y->right;
+        if (y->right) y->right->parent = x;
+        y->parent = x->parent;
+
+        if (!x->parent) r = y;
+        else if (x == x->parent->left) x->parent->left = y;
+        else x->parent->right = y;
+
+        y->right = x;
+        x->parent = y;
+    }
+
+    void fix(Node*& r, Node*& x) {
+        while (x != r && x->parent->color == RED) {
+            Node* p = x->parent;
+            Node* g = p->parent;
+
+            if (p == g->left) {
+                Node* u = g->right;
+                if (u && u->color == RED) {
+                    g->color = RED;
+                    p->color = BLACK;
+                    u->color = BLACK;
+                    x = g;
+                } else {
+                    if (x == p->right) {
+                        leftRotate(r, p);
+                        x = p;
+                        p = x->parent;
+                    }
+                    rightRotate(r, g);
+                    Color t = p->color;
+                    p->color = g->color;
+                    g->color = t;
+                    x = p;
+                }
